7dd80f6c8337f5bfb07e0de14f54236c84490fbf
James Priest <james.priest@everbridge.com>
2019-12-27 16:23:43 -0800

    feat: Add CsvFileReader

---
+++ src/MatchResult.ts
@@ -0,0 +1,7 @@
+// Use enum to signal to other devs that these are a set
+//  of closely related objects
+export enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
-// .map((row) => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+// const dateOfFirstMatch = reader.data[0][0];
+
+console.log('reader.data', reader.data);
 
 let manUnitedWins = 0;
 
-for (const match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+for (const match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
