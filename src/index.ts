12d95cb4a8d8041dcbab1ed7b4e15c92ba0228f9
James Priest <james-priest@outlook.com>
2019-12-30 18:00:59 -0800

    refactor: Create static methods

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReports';
 
-// 1. Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-// 2. Create an instance of MatchReader and pass in something
-//    satisfying the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  // new ConsoleReport()
-  new HtmlReport()
-);
-
 summary.buildAndPrintReport(matchReader.matches);
